6090023a499459bc304132dca200b39d5f212870
myy <mayingyun2014@163.com>
2016-10-06 16:11:57 +0800

    添加参考

--- UIStackViewDemo/UIStackViewDemo/AppDelegate.h
+++ UIStackViewDemo/UIStackViewDemo/AppDelegate.h
@@ -6,6 +6,8 @@
 //  Copyright © 2016年 zzedu. All rights reserved.
 //
 
+//参考 http://www.cocoachina.com/ios/20150623/12233.html
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
